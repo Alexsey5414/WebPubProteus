c36bfb35df11a3295dccc92023ba59bc9bec5b05
alexsey <golubev_alexsey@mail.ru>
2022-02-09 17:02:56 +0300

    Добаление файла Delayer

--- Classes/Delayer.cs
+++ Classes/Delayer.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using WebTestProteus.Interfaces;
 
 namespace WebTestProteus.Classes
 {
-    public class Delayer
+     public class Dalayer : IDelayer, IDisposable
     {
+        public delegate void DalayerHandler(ReportMetric rm);
+        public event DalayerHandler OnDelay;
+        Stopwatch sw;
+        ReportMetric _rm;
+        public Dalayer(ReportMetric rm)
+        {
+            _rm = rm;
+            sw = Stopwatch.StartNew();
+
+        }
+        public void Dispose()
+        {
+            sw.Stop();
+            _rm.Duration = sw.ElapsedMilliseconds;
+            OnDelay?.Invoke(_rm);
+        }
+
     }
+
 }
